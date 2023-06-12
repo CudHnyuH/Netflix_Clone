c45921547d0a4201d76d7e38941160f65a50d503
cudhnyuh <phamhuynhduc805@gmail.com>
2023-06-12 21:12:31 +0700

    Google and Github OAuth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { signIn } from 'next-auth/react';
 import { useRouter } from "next/router";
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 const Auth = () => {
     const router = useRouter();
 
@@ -31,7 +34,7 @@ const Auth = () => {
             console.log(error);
         }
     },[email, password, router])
-    
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -128,6 +131,44 @@ const Auth = () => {
                         >
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div 
+                                onClick={() => signIn('google', { callbackUrl: '/' })} 
+                                className="
+                                    w-10 
+                                    h-10 
+                                    bg-white 
+                                    rounded-full 
+                                    flex 
+                                    items-center 
+                                    justify-center 
+                                    cursor-pointer 
+                                    hover:opacity-80 
+                                    transition
+                                "
+                            >
+                                <FcGoogle size={32} />
+                            </div>
+                            <div 
+                                onClick={() => signIn('github', { callbackUrl: '/' })} 
+                                className="
+                                    w-10 
+                                    h-10 
+                                    bg-white 
+                                    rounded-full 
+                                    flex 
+                                    items-center 
+                                    justify-center 
+                                    cursor-pointer 
+                                    hover:opacity-80 
+                                    transition
+                                "
+                            >
+                                <FaGithub size={32} />
+                            </div>
+                        </div>
+
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                             <span 
